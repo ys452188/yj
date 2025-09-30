847cebe8eefb7067c430d4dc9933fc0c5f3a4b66
ys452188 <minhuachen167@gmail.com>
2025-09-30 13:07:38 +0800

    初次提交

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log( "来~让我康康" );
+function warning_continue(){  //警告弹窗下的"继续"按钮
+    console.log("登dua~郎")
+	var element = document.getElementById("warning");
+	element.style.display = 'none';
+}
