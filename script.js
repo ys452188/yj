65f1bee1ff57edaa212c445ac970f6b965830654
ys452188 <minhuachen167@gmail.com>
2025-09-30 13:17:56 +0800

    稍加修改

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function warning_continue(){  //警告弹窗下的"继续"按钮
 	var element = document.getElementById("warning");
 	element.style.display = 'none';
 }
+function warning_exit(){  //警告弹窗下的"关闭"按钮(可能无效)
+    window.opener = null;
+    window.open('', '_self').close();
+}
