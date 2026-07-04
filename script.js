5e50171f31d5e016179e317418972275fdeb4f3b
硫酸铜非常好吃 <147019024+ys452188@users.noreply.github.com>
2026-07-04 16:18:15 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function warning_exit() { //警告弹窗下的"关闭"按钮(可能无效)
 }
 
 
-const URL = "https://sheep-dairy-studying-double.trycloudflare.com"
+const URL = "https://kissing-jerusalem-lite-constitution.trycloudflare.com"
 const DELAY = 60; //刷新间隔
 
 var name;
